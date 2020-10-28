7539d5e7656a8457783e485e273001cb4edcbb7e
Julien Blondeau <julienblondeau@live.fr>
2020-10-28 16:49:51 +0100

    can't send form if there is an empty field

--- add.php
+++ add.php
@@ -1,9 +1,27 @@
 <?php 
 
 	if(isset($_POST['submit'])){
-		echo htmlspecialchars($_POST['email']);
 		echo htmlspecialchars($_POST['title']);
-		echo htmlspecialchars($_POST['ingredients']);
+    echo htmlspecialchars($_POST['ingredients']);
+
+    
+    if(empty($_POST['email'])) {
+      echo 'An email is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['email']);
+    }
+
+    if(empty($_POST['title'])) {
+      echo 'A title is required <br />';
+    } else {
+      echo htmlspecialchars($_POST['title']);
+    }
+
+    if(empty($_POST['ingredients'])) {
+      echo 'Some ingredients are required <br />';
+    } else {
+      echo htmlspecialchars($_POST['ingredients']);
+    }
 	}
 
 ?>
