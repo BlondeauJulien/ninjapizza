39212aa58f371359ed46d72331bfc1bebace2118
Julien Blondeau <julienblondeau@live.fr>
2020-10-28 22:15:48 +0100

    delete pizza

--- details.php
+++ details.php
@@ -2,6 +2,20 @@
 
   include('config/db_connect.php');
 
+  if(isset($_POST['delete'])){
+
+		$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+		$sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+		if(mysqli_query($conn, $sql)){
+			header('Location: index.php');
+		} else {
+			echo 'query error: '. mysqli_error($conn);
+		}
+
+	}
+
   if(isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
 
@@ -13,13 +27,7 @@
 
     mysqli_free_result($result);
     mysqli_close($conn);
-
-    print_r($pizza);
   }
-
-
-
-
 
 ?>
 
@@ -34,10 +42,18 @@
 			<p><?php echo date($pizza['created_at']); ?></p>
 			<h5>Ingredients:</h5>
 			<p><?php echo $pizza['ingredients']; ?></p>
+
+        <!-- DELETE FORM -->
+      <form action="details.php" method="POST">
+        <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+        <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+      </form>
+
 		<?php else: ?>
 			<h5>No such pizza exists.</h5>
 		<?php endif ?>
 	</div>
+
 
   <?php include('templates/footer.php') ?>
 </html>
