e04a226b27c96ac105b6012c9252fb86ffe68593
Julien Blondeau <julienblondeau@live.fr>
2020-10-28 18:20:04 +0100

    request pizzas from database

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
     echo 'Connection error: ' . mysqli_connect_error();
   }
 
+  // write query for all pizzas
+  $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+  // make query & get result
+  $result = mysqli_query($conn, $sql);
+
+  // fetch the resulting rows as an array
+  $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  //free from memory
+  mysqli_free_result($result);
+
+  //close connection
+  mysqli_close($conn);
 
 ?>
 
