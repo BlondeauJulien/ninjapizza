f257396cbf29a7d859925d1d3bd0408597b2230a
Julien Blondeau <julienblondeau@live.fr>
2020-10-28 18:11:48 +0100

    set up MySQL database

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php 
 
+  // connect to database (MySQLi)
+  $conn = mysqli_connect('localhost', 'julien', 'azerty', 'ninja_pizza');
+
+  //check database connection
+  if(!$conn) {
+    echo 'Connection error: ' . mysqli_connect_error();
+  }
+
 
 ?>
 
