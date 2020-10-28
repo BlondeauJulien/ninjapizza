82d8409a8f962bc509826af799bcb1bb88c29c09
Julien Blondeau <julienblondeau@live.fr>
2020-10-28 17:26:38 +0100

    redirect if submited form is valid

--- add.php
+++ add.php
@@ -30,7 +30,13 @@
 			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
 				$errors['ingredients'] = 'Ingredients must be a comma separated list';
 			}
-		}
+    }
+    
+    if(array_filter($errors)) {
+
+    } else {
+      header('Location: index.php');
+    }
 
 	}
 
@@ -44,18 +50,23 @@
 	<section class="container grey-text">
 		<h4 class="center">Add a Pizza</h4>
 		<form class="white" action="add.php" method="POST">
+
 			<label>Your Email</label>
 			<input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
 			<div class="red-text"><?php echo $errors['email']; ?></div>
+
 			<label>Pizza Title</label>
 			<input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
 			<div class="red-text"><?php echo $errors['title']; ?></div>
+
 			<label>Ingredients (comma separated)</label>
 			<input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
 			<div class="red-text"><?php echo $errors['ingredients']; ?></div>
+
 			<div class="center">
 				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
 			</div>
+
 		</form>
 	</section>
 
